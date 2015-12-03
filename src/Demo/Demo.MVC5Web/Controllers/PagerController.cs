c0bb09c641e158f2038e6d350e007a0f40070317
heycoder <mail@keshuang.net>
2015-12-03 15:04:06 +0800

    添加demo

---
+++ src/Demo/Demo.MVC5Web/Controllers/PagerController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo.MVC5Web.Controllers
+{
+    public class PagerController : Controller
+    {
+        //
+        // GET: /Pager/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
